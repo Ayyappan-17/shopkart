7c5c53102d1db2a49d5339c561a14903292e3b08
Ayyappan <s.ayyappan2003@gmail.com>
2026-04-15 02:35:48 +0530

    changed springboot version and modification in securityconfig and userdetailservice

---
+++ src/main/java/com/shopkart/shopkart/security/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.shopkart.shopkart.security;
+
+import com.shopkart.shopkart.entity.User;
+import com.shopkart.shopkart.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+        return new CustomUserDetails(user);
+    }
+}
